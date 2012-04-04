8bc6eb6529c7d2e8f40ceedc51e3ec6ddf6d6f35
Thomas Zebühr <thomas_andaliten@hotmail.com>
2012-04-04 12:15:29 +0200

    Added a few gp-operator class stubs

---
+++ src/gp/IfDangerLeft.java
@@ -0,0 +1,23 @@
+package gp;
+
+import ec.*;
+import ec.gp.*;
+import ec.util.*;
+
+public class IfDangerLeft extends GPNode
+{
+
+	@Override
+	public String toString()
+	{
+		return "ifDangerLeft";
+	}
+
+	@Override
+	public void eval(EvolutionState state, int thread, GPData input,
+					 ADFStack stack, GPIndividual individual, Problem problem)
+	{
+		;
+	}
+	
+}
---
+++ src/gp/IfFoodAhead.java
@@ -0,0 +1,23 @@
+package gp;
+
+import ec.*;
+import ec.gp.*;
+import ec.util.*;
+
+public class IfFoodAhead extends GPNode
+{
+
+	@Override
+	public String toString()
+	{
+		return "ifFoodAhead";
+	}
+
+	@Override
+	public void eval(EvolutionState state, int thread, GPData input,
+					 ADFStack stack, GPIndividual individual, Problem problem)
+	{
+		;
+	}
+	
+}
