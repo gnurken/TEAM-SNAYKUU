cde4a072fc87f6ca226e24583f70eb852e8f9836
Daniel Malmqvist <1ma.swe@gmail.com>
2012-04-04 12:11:49 +0200

    I've added the package gp with the leafnodes that will be needed

---
+++ src/gp/Left.java
@@ -0,0 +1,22 @@
+package gp;
+
+import ec.EvolutionState;
+import ec.Problem;
+import ec.gp.*;
+
+public class Left extends GPNode
+{
+	@Override
+	public String toString() 
+	{
+		return "Left";
+	}
+
+	@Override
+	public void eval(EvolutionState state, int thread, GPData input,
+			ADFStack stack, GPIndividual individual, Problem problem) 
+	{
+		// TODO Auto-generated method stub	
+	}
+
+}
