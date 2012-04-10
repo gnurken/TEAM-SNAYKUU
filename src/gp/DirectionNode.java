af554402ca48639437b0c0aa5f66fefaefcf8650
Thomas Zebuhr <thomas_andaliten@hotmail.com>
2012-04-10 11:45:14 +0200

    Extended DirectionNode from ERC, so that it can be used to hold a random direction constant.

--- src/gp/DirectionNode.java
+++ src/gp/DirectionNode.java
@@ -4,33 +4,72 @@ import ec.EvolutionState;
 import ec.Problem;
 import ec.gp.*;
 import ec.util.Parameter;
+import gameLogic.Direction;
 
-public class DirectionNode extends GPNode
+public class DirectionNode extends ERC
 {
+	
+	private Direction direction;
 
 	@Override
 	public String toString() 
 	{
-		return "direction";
+		return "direction(" + direction.toString() + ")";
 	}
 
+	@Override
     public void checkConstraints(final EvolutionState state,
             final int tree,
             final GPIndividual typicalIndividual,
             final Parameter individualBase)
 	{
-	super.checkConstraints(state,tree,typicalIndividual,individualBase);
-	if (children.length!=0)
-	state.output.error("Incorrect number of children for node " + 
-	          toStringForError() + " at " +
-	          individualBase);
+		super.checkConstraints(state,tree,typicalIndividual,individualBase);
+		if (children.length!=0)
+		state.output.error("Incorrect number of children for node " + 
+							toStringForError() + " at " +
+							individualBase);
 	}
 	
 	@Override
 	public void eval(EvolutionState state, int thread, GPData input,
 			ADFStack stack, GPIndividual individual, Problem problem) 
 	{
+		DirectionData data = (DirectionData)input;
+		data.dir = direction;
+	}
+
+	@Override
+	public void resetNode(EvolutionState state, int thread)
+	{
+		int n = state.random[thread].nextInt(4);
 		
+		switch (n)
+		{
+		case 0:
+			direction = Direction.EAST;
+			break;
+		case 1:
+			direction = Direction.WEST;
+			break;
+		case 2:
+			direction = Direction.EAST;
+			break;
+		case 3:
+			direction = Direction.NORTH;
+			break;
+		}
+	}
+
+	@Override
+	public boolean nodeEquals(GPNode node)
+	{
+		return (node instanceof DirectionNode && ((DirectionNode)node).direction == direction);
+	}
+
+	@Override
+	public String encode()
+	{
+		return toString();
 	}
 	
 }
