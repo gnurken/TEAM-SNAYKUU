105b586b9b4ce55ed831e6a3fd6d7576699e2520
Thomas Zebuhr <thomas_andaliten@hotmail.com>
2012-04-10 17:17:17 +0200

    Fleshed out the implementation of SnaykuuProblem. Hasn't yet been tested, but as far as I can tell it should be able to do all that it should, except evaluate the fitness.

--- src/gp/SnaykuuProblem.java
+++ src/gp/SnaykuuProblem.java
@@ -1,9 +1,28 @@
 package gp;
 
+import java.awt.Color;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Set;
+import java.util.TreeSet;
+
+import bot.Cancer;
+import bot.CarefulBot;
+import bot.FruitEaterBot;
+
 import ec.EvolutionState;
 import ec.Individual;
+import ec.gp.GPIndividual;
+import ec.gp.GPNode;
 import ec.gp.GPProblem;
+import ec.gp.koza.KozaFitness;
 import ec.util.Parameter;
+import gameLogic.Brain;
+import gameLogic.GameObjectType;
+import gameLogic.Metadata;
+import gameLogic.Session;
+import gameLogic.Snake;
+import gameLogic.Team;
 
 public class SnaykuuProblem extends GPProblem
 {
@@ -19,19 +38,62 @@ public class SnaykuuProblem extends GPProblem
 	
 	public void setup(final EvolutionState state, final Parameter base)
 	{
-	super.setup(state, base);
-	
-	input = (DirectionData) state.parameters.getInstanceForParameterEq(
-							base.push(P_DATA), null, DirectionData.class);
-	input.setup(state,base.push(P_DATA));
+		super.setup(state, base);
+		
+		input = (DirectionData) state.parameters.getInstanceForParameterEq(
+								base.push(P_DATA), null, DirectionData.class);
+		input.setup(state, base.push(P_DATA));
+		
 	}
 
 	@Override
 	public void evaluate(EvolutionState state, Individual ind,
 						 int subpopulation, int threadnum)
 	{
+		
 		if (!ind.evaluated)
 		{
+			GameObjectType objectType = new GameObjectType("Snake", true);
+			
+			Metadata metadata = null;
+			Session session = new Session(metadata);
+			
+			Team contestants = new Team("Contestants", 1);
+			session.addTeam(contestants);
+			
+			GPIndividual individual = (GPIndividual)ind;
+			int snakesPerTeam = individual.trees.length;
+			for (int i = 0; i < snakesPerTeam; ++i)
+			{
+				GPNode root = individual.trees[i].child;
+				
+				GPBrain brain = new GPBrain();
+				brain.setup(root, state, threadnum, stack, individual, this, input);
+				
+				Snake snake = new Snake(objectType, "Contestant" + i, brain, Color.BLUE);
+				session.addSnake(snake, contestants);
+			}
+			
+			// Create Opponent team
+			Team opponents = new Team("Opponents", 2);
+			session.addTeam(opponents);
+			
+			for (int i = 0; i < snakesPerTeam; ++i)
+			{
+				Snake snake = new Snake(objectType, "Opponent" + i, new FruitEaterBot(), Color.RED);
+				session.addSnake(snake, opponents);
+			}
+			
+			session.prepareForStart();
+			
+			while (!session.hasEnded())
+				session.tick();
+			
+			List<Team> result = session.getGameResult().getTeams();
+			
+			// TODO: evaluate fitness
+			((KozaFitness)ind.fitness).setStandardizedFitness(state, 1.0f);
+			
 			ind.evaluated = true;
 		}
 	}
