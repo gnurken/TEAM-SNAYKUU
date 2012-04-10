dc8502c8e158f97d1394b5df3e9232019b4f5fe3
Thomas Zebuhr <thomas_andaliten@hotmail.com>
2012-04-10 13:21:31 +0200

    Started implementation of the GPBrain class.

---
+++ src/gp/GPBrain.java
@@ -0,0 +1,42 @@
+package gp;
+
+import ec.EvolutionState;
+import ec.gp.ADFStack;
+import ec.gp.GPIndividual;
+import ec.gp.GPNode;
+import gameLogic.Brain;
+import gameLogic.Direction;
+import gameLogic.GameState;
+import gameLogic.Snake;
+
+public class GPBrain implements Brain
+{
+	
+	private GPNode root;
+	private EvolutionState state;
+	private int thread;
+	private ADFStack stack;
+	private GPIndividual individual;
+	private SnaykuuProblem problem;
+	private DirectionData input;
+	
+	public void setup(GPNode root, EvolutionState state, int thread, ADFStack stack,
+					  GPIndividual individual, SnaykuuProblem problem, DirectionData input)
+	{
+		this.root = root;
+		this.thread = thread;
+		this.stack = stack;
+		this.individual = individual;
+		this.problem = problem;
+		this.input = input;
+	}
+
+	@Override
+	public Direction getNextMove(Snake yourSnake, GameState gameState)
+	{
+		root.eval(state, thread, input, stack, individual, problem);
+		
+		return input.dir;
+	}
+
+}
