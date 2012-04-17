fb1776833a8cc5a83e16334b019e861a20715a0c
Daniel Malmqvist <1ma.swe@gmail.com>
2012-04-17 11:52:31 +0200

    updated the main to take program arguments for writing all generations full decision trees

--- src/gp/Main.java
+++ src/gp/Main.java
@@ -6,7 +6,7 @@ public class Main {
 	
 	public static void main(String[] args)
 	{
-		String evolveArgs[] = {"-file", "params/snaykuu.params"};
+		String evolveArgs[] = {"-file", "params/snaykuu.params", "-p", "stat.gather-full=true"};
 		
 		Evolve.main(evolveArgs);
 	}
