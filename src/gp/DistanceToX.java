c95dd76f7df64df522fb6f1941adad8661964592
Daniel Malmqvist <1ma.swe@gmail.com>
2012-04-17 15:53:59 +0200

    added distancetoX and Y decision nodes

---
+++ src/gp/DistanceToX.java
@@ -0,0 +1,36 @@
+package gp;
+
+import ec.EvolutionState;
+import ec.Problem;
+import ec.gp.ADFStack;
+import ec.gp.GPData;
+import ec.gp.GPIndividual;
+import ec.gp.GPNode;
+import gameLogic.Position;
+import gp.IntData;
+import gp.PositionData;
+import gp.SnaykuuProblem;
+
+public class DistanceToX extends GPNode
+{
+
+	@Override
+	public String toString() 
+	{
+		return "DistanceToX";
+	}
+
+	@Override
+	public void eval(EvolutionState state, int thread, GPData input,
+			ADFStack stack, GPIndividual individual, Problem problem) 
+	{
+		IntData data = (IntData)input;
+		
+		PositionData posData = new PositionData();
+		Position pos = ((SnaykuuProblem)problem).getActiveSnake().getHeadPosition();
+		
+		children[0].eval(state, thread, posData, stack, individual, problem);
+		
+		data.value = Math.abs(pos.getX() - posData.pos.getX());
+	}
+}
