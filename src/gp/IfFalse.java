6604aa7947cef08d45ab6d5b798aefd074a2b9e1
Thomas Zebuhr <thomas_andaliten@hotmail.com>
2012-04-17 12:09:47 +0200

    Added the IfTrue and IfFalse GPNodes, which take a bool as the first child (the condition), and evaluates either the second or third child, depending on the condition.

---
+++ src/gp/IfFalse.java
@@ -0,0 +1,32 @@
+package gp;
+
+import ec.EvolutionState;
+import ec.Problem;
+import ec.gp.ADFStack;
+import ec.gp.GPData;
+import ec.gp.GPIndividual;
+import ec.gp.GPNode;
+
+public class IfFalse extends GPNode
+{
+
+	@Override
+	public String toString()
+	{
+		return "If";
+	}
+
+	@Override
+	public void eval(EvolutionState state, int thread, GPData input,
+			ADFStack stack, GPIndividual individual, Problem problem)
+	{
+		BoolData data = new BoolData();
+		children[0].eval(state, thread, data, stack, individual, problem);
+		
+		if (!data.bool)
+			children[1].eval(state, thread, input, stack, individual, problem);
+		else
+			children[2].eval(state, thread, input, stack, individual, problem);
+	}
+
+}
