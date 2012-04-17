84de82ac9ed4b1de63bc5304078a8f8c0dcf9511
Thomas Zebuhr <thomas_andaliten@hotmail.com>
2012-04-17 11:17:12 +0200

    put curly brackets on their own line

--- src/gp/RoundsUntilGrowth.java
+++ src/gp/RoundsUntilGrowth.java
@@ -8,16 +8,19 @@ import ec.gp.GPIndividual;
 import ec.gp.GPNode;
 import gameLogic.Session;
 
-public class RoundsUntilGrowth extends GPNode {
+public class RoundsUntilGrowth extends GPNode
+{
 
 	@Override
-	public String toString() {
+	public String toString()
+	{
 		return "RoundsUntilGrowth";
 	}
 
 	@Override
 	public void eval(EvolutionState state, int thread, GPData input,
-			ADFStack stack, GPIndividual individual, Problem problem) {
+			ADFStack stack, GPIndividual individual, Problem problem)
+	{
 		IntData data = (IntData)input;
 		Session session = ((SnaykuuProblem)problem).getSession();
 		data.value = session.getGameResult().getRecordedGame().getTurnCount() % session.getMetadata().getFruitFrequency();
