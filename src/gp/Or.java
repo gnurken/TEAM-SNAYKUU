459a413d453aff9189d69b2bbadbbe449f0c70b5
Thomas Zebuhr <thomas_andaliten@hotmail.com>
2012-04-17 15:46:31 +0200

    Added the GPNodes And and Or, should be obvious what they do

---
+++ src/gp/Or.java
@@ -0,0 +1,32 @@
+package gp;
+
+import ec.EvolutionState;
+import ec.Problem;
+import ec.gp.ADFStack;
+import ec.gp.GPData;
+import ec.gp.GPIndividual;
+import ec.gp.GPNode;
+
+public class Or extends GPNode
+{
+
+	@Override
+	public String toString()
+	{
+		return "And";
+	}
+
+	@Override
+	public void eval(EvolutionState state, int thread, GPData input,
+			ADFStack stack, GPIndividual individual, Problem problem)
+	{
+		BoolData data = (BoolData)input;
+		children[0].eval(state, thread, data, stack, individual, problem);
+		boolean left = data.bool;
+		children[1].eval(state, thread, data, stack, individual, problem);
+		boolean right = data.bool;
+		
+		data.bool = left || right;
+	}
+
+}
