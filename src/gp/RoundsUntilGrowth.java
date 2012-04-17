2571fcbc717925262ae9cc4583bf05e25162e897
Thomas Zebuhr <thomas_andaliten@hotmail.com>
2012-04-17 11:00:38 +0200

    Added RoundUntilGrowth GPNode, it does what it says in the name

---
+++ src/gp/RoundsUntilGrowth.java
@@ -0,0 +1,26 @@
+package gp;
+
+import ec.EvolutionState;
+import ec.Problem;
+import ec.gp.ADFStack;
+import ec.gp.GPData;
+import ec.gp.GPIndividual;
+import ec.gp.GPNode;
+import gameLogic.Session;
+
+public class RoundsUntilGrowth extends GPNode {
+
+	@Override
+	public String toString() {
+		return "RoundsUntilGrowth";
+	}
+
+	@Override
+	public void eval(EvolutionState state, int thread, GPData input,
+			ADFStack stack, GPIndividual individual, Problem problem) {
+		IntData data = (IntData)input;
+		Session session = ((SnaykuuProblem)problem).getSession();
+		data.value = session.getGameResult().getRecordedGame().getTurnCount() % session.getMetadata().getFruitFrequency();
+	}
+
+}
