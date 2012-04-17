ab39b34b351a785bf445a9c1550d235a9b385f39
Thomas Zebuhr <thomas_andaliten@hotmail.com>
2012-04-17 11:18:24 +0200

    Created the int comparison nodes LessThan, GreaterThan and EqualTo.

---
+++ src/gp/GreaterThan.java
@@ -0,0 +1,31 @@
+package gp;
+
+import ec.EvolutionState;
+import ec.Problem;
+import ec.gp.ADFStack;
+import ec.gp.GPData;
+import ec.gp.GPIndividual;
+import ec.gp.GPNode;
+
+public class GreaterThan extends GPNode
+{
+
+	@Override
+	public String toString()
+	{
+		return ">";
+	}
+
+	@Override
+	public void eval(EvolutionState state, int thread, GPData input,
+			ADFStack stack, GPIndividual individual, Problem problem)
+	{
+		IntData data = new IntData();
+		children[0].eval(state, thread, data, stack, individual, problem);
+		int left = data.value;
+		children[1].eval(state, thread, data, stack, individual, problem);
+		int right = data.value;
+		((BoolData)input).bool = left > right;
+	}
+
+}
