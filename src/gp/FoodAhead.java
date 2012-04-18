1a39643ce481ca186ee209791f4c9d62ddd321d3
Daniel Malmqvist <1ma.swe@gmail.com>
2012-04-18 15:57:33 +0200

    added the food in direction decision nodes

---
+++ src/gp/FoodAhead.java
@@ -0,0 +1,32 @@
+package gp;
+
+import ec.*;
+import ec.gp.*;
+import gameLogic.Position;
+import gameLogic.Snake;
+import gameLogic.Square;
+
+public class FoodAhead extends GPNode
+{
+
+	@Override
+	public String toString()
+	{
+		return "FoodAhead";
+	}
+
+	@Override
+	public void eval(EvolutionState state, int thread, GPData input,
+					 ADFStack stack, GPIndividual individual, Problem problem)
+	{
+		BoolData data = (BoolData)input;
+		
+		Snake snake = ((SnaykuuProblem)problem).getActiveSnake();
+		
+		Position pos = snake.getCurrentDirection().calculateNextPosition(snake.getHeadPosition());
+		
+		Square square = ((SnaykuuProblem)problem).getSession().getBoard().getSquare(pos);
+		
+		data.bool = square.hasFruit();
+	}
+}
