9865f2452126f005ecbc09c016d46bce7776f795
Daniel Malmqvist <1ma.swe@gmail.com>
2012-04-18 15:57:08 +0200

    added the implemented danger in direction decision nodes

---
+++ src/gp/DangerLeft.java
@@ -0,0 +1,32 @@
+package gp;
+
+import ec.*;
+import ec.gp.*;
+import gameLogic.Position;
+import gameLogic.Snake;
+import gameLogic.Square;
+
+public class DangerLeft extends GPNode
+{
+
+	@Override
+	public String toString()
+	{
+		return "DangerLeft";
+	}
+
+	@Override
+	public void eval(EvolutionState state, int thread, GPData input,
+					 ADFStack stack, GPIndividual individual, Problem problem)
+	{
+		BoolData data = (BoolData)input;
+		
+		Snake snake = ((SnaykuuProblem)problem).getActiveSnake();
+		
+		Position pos = snake.getCurrentDirection().turnLeft().calculateNextPosition(snake.getHeadPosition());
+		
+		Square square = ((SnaykuuProblem)problem).getSession().getBoard().getSquare(pos);
+		
+		data.bool = square.isLethal();
+	}
+}
